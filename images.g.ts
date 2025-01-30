08194dbf70a57d16a93cb7e194661f5cb82adec0
Sverre02 <sverre.rommetveit.landheim@gmail.com>
2025-01-30 18:59:54 +0100

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,177 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image8":
+            case "Zelda back alt":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . f c c c c f . . . . . 
+. . . . f c c c c c c f . . . . 
+. . . f c c c f c c c f f . . . 
+. . . f c c c c f c c c f . . . 
+. . . f c c c c c c c c f . . . 
+. . . f a c c c c c c c f . . . 
+. . . f f a c c c c c f f . . . 
+. . . f c f 6 6 c 6 6 f c f . . 
+. . f d a f c 6 c 6 c f a f . . 
+. . f d f c 6 6 c 6 6 c f f . . 
+. . . f a c 6 c c c 6 c f . . . 
+. . . f f a 6 6 c 6 f f f . . . 
+. . . f f f f f f f e e f f . . 
+. . . . f f f f f f f f f . . . 
+`;
+            case "image1":
+            case "Zelda left":return img`
+. . . . . f f f f f . . . . . . 
+. . . f f c c c a a f . . . . . 
+. . f 5 5 c c c c c a f . . . . 
+. . f 5 5 5 c c c c c a f . . . 
+. . f f 5 5 5 c c c c c a f . . 
+. . . f f f 5 c c c c c c f . . 
+. f f d f b f f c c c c f . . . 
+. f d d f d b f c c c f . . . . 
+. . f d d d b f c 6 f . . . . . 
+. . . f 3 d f f c 6 f . . . . . 
+. . . f f f 1 1 f 6 f . . . . . 
+. . . f 3 f 1 1 f c f . . . . . 
+. . . f 3 f d d f c f . . . . . 
+. . . f 3 f f f f f f . . . . . 
+. . . f f f e e e e f . . . . . 
+. . f f f f f f f f f f . . . . 
+`;
+            case "image5":
+            case "Zelda left alt":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . f f c c c a a f . . . . . 
+. . f 5 5 c c c c c a f . . . . 
+. . f 5 5 5 c c c c c a f . . . 
+. . f f 5 5 5 c c c c c a f . . 
+. . . f f f 5 c c c c c c f . . 
+. f f d f b f f c c c c f . . . 
+. f d d f d b f c c f f . . . . 
+. . f 3 d d b f c 6 f . . . . . 
+. . . f f f f f c 6 f . . . . . 
+. . . . f 1 1 f c 6 f f . . . . 
+. . . . f d d f c 6 f e f . . . 
+. . . f f f f c c f f e f . . . 
+. . f e e e e f f f e e f . . . 
+. . f f f f f f f f f f f f . . 
+`;
+            case "image6":
+            case "Zelda right alt":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f f . . . . . 
+. . . . . f a a c c c f f . . . 
+. . . . f a c c c c c 5 5 f . . 
+. . . f a c c c c c 5 5 5 f . . 
+. . f a c c c c c 5 5 5 f f . . 
+. . f c c c c c c 5 f f f . . . 
+. . . f c c c c f f b f d f f . 
+. . . . f f c c f b d f d d f . 
+. . . . . f 6 c f b d d 3 f . . 
+. . . . . f 6 c f f f f f . . . 
+. . . . f f 6 c f 1 1 f . . . . 
+. . . f e f 6 c f d d f . . . . 
+. . . f e f f c c f f f f . . . 
+. . . f e e f f f e e e e f . . 
+. . f f f f f f f f f f f f . . 
+`;
+            case "image7":
+            case "Zelda back":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . f c c c c f . . . . . 
+. . . . f c c c c c c f . . . . 
+. . . f f c c c f c c c f . . . 
+. . . f c c c f c c c c f . . . 
+. . . f c c c c c c c c f . . . 
+. . . f c c c c c c c a f . . . 
+. . . f f c c c c c a f f . . . 
+. . f c f 6 6 c 6 6 f c f . . . 
+. . f a f c 6 c 6 c f a d f . . 
+. . f f c 6 6 c 6 6 c f d f . . 
+. . . f c 6 c c c 6 c a f . . . 
+. . . f f f 6 c 6 6 a f f . . . 
+. . f f e e f f f f f f f . . . 
+. . . f f f f f f f f f . . . . 
+`;
+            case "image3":
+            case "zelda right":return img`
+. . . . f f f f f . . . . . . . 
+. . . f a a c c c f f . . . . . 
+. . f a c c c c c 5 5 f . . . . 
+. f a c c c c c 5 5 5 f . . . . 
+f a c c c c c 5 5 5 f f . . . . 
+f c c c c c c 5 f f f . . . . . 
+. f c c c c f f b f d f f . . . 
+. . f c c c f b d f d d f . . . 
+. . . f 6 c f b d d d f . . . . 
+. . . f 6 c f f d 3 f . . . . . 
+. . . f 6 f 1 1 f f f . . . . . 
+. . . f c f 1 1 f 3 f . . . . . 
+. . . f c f d d f 3 f . . . . . 
+. . . f f f f f f 3 f . . . . . 
+. . . f e e e e f f f . . . . . 
+. . f f f f f f f f f f . . . . 
+`;
+            case "image2":
+            case "Zelda front":return img`
+. . . . . f f f f f f . . . . . 
+. . . . f a c c c c c f . . . . 
+. . . f a c c c c c c c f . . . 
+. . . f a c 5 5 c c c c f . . . 
+. . f a c 5 5 5 5 5 c c c f . . 
+. . f c c 5 d d d d 5 5 c f . . 
+. . f c 5 d f d d f d 5 c f . . 
+. . f c 5 d f d d f d 5 c f . . 
+. . . f c d d d d d d c f . . . 
+. . f 1 f c d 3 3 d c f 1 f . . 
+. . f 1 f f f f f f f f 1 d f . 
+. . f 1 d d f 1 1 a a f f d f . 
+. . . f d d f 1 1 a a f f f . . 
+. . . . f f a 1 1 f f f f . . . 
+. . . f f f f f f e e e f f . . 
+. . . . f f f f f f f f f . . . 
+`;
+            case "image4":
+            case "Zelda front alt":return img`
+. . . . . f f f f f f . . . . . 
+. . . . f a c c c c c f . . . . 
+. . . f a c c c c c c c f . . . 
+. . . f c c c 5 5 c c c f . . . 
+. . f a c c 5 5 5 5 5 c c f . . 
+. . f c c 5 d d d d 5 5 c f . . 
+. . f c 5 d f d d f d 5 c f . . 
+. . f c 5 d f d d f d 5 c f . . 
+. . . f c d d d d d d c f . . . 
+. . f 1 f c d 3 3 d c f 1 f . . 
+. f d 1 f f f f f f f f 1 f . . 
+. f d f f a a 1 1 f d d 1 f . . 
+. . f f f a a 1 1 f d d f . . . 
+. . . f f f f 1 1 a f f . . . . 
+. . f f e e e f f f f f f . . . 
+. . . f f f f f f f f f . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
