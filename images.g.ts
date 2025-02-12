4de53c0104c439dd18f1e149f3a592e343034fd8
Sverre02 <sverre.rommetveit.landheim@gmail.com>
2025-02-12 17:13:44 +0100

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -136,25 +136,6 @@ namespace myImages {
 . . . f f f e e e e f . . . . . 
 . . f f f f f f f f f f . . . . 
 `;
-            case "image3":
-            case "zelda_right":return img`
-. . . . f f f f f . . . . . . . 
-. . . f a a c c c f f . . . . . 
-. . f a c c c c c 5 5 f . . . . 
-. f a c c c c c 5 5 5 f . . . . 
-f a c c c c c 5 5 5 f f . . . . 
-f c c c c c c 5 f f f . . . . . 
-. f c c c c f f b f d f f . . . 
-. . f c c c f b d f d d f . . . 
-. . . f 6 c f b d d d f . . . . 
-. . . f 6 c f f d 3 f . . . . . 
-. . . f 6 f 1 1 f f f . . . . . 
-. . . f c f 1 1 f a f . . . . . 
-. . . f c f d d f a f . . . . . 
-. . . f f f f f f a f . . . . . 
-. . . f e e e e f f f . . . . . 
-. . f f f f f f f f f f . . . . 
-`;
             case "image9":
             case "Gust_zol":return img`
 . . . . . . . . . . . . . . . . 
@@ -174,56 +155,176 @@ f c c c c c c 5 f f f . . . . .
 . . . f f 5 5 5 5 5 5 f f . . . 
 . . . . f f f f f f f f . . . . 
 `;
+            case "image11":
+            case "Zelda_front_stone":return img`
+. . . . . f f f f f f . . . . . 
+. . . f f a c c c c c f f . . . 
+. . f d d f c c c c f d d f . . 
+. . f d d f 5 5 c c f d d f . . 
+. . f f f 5 5 5 5 5 c f f f . . 
+. . f 1 f 5 d d d d 5 f 1 f . . 
+. . f 1 f d f d d f d f 1 f . . 
+. . f 1 f d f d d f d f 1 f . . 
+. . f 1 f d d d d d d f 1 f . . 
+. . . f f c d 3 3 d c f 1 f . . 
+. . . f a f f f f f f a f . . . 
+. . . f a a a 1 1 a a a f . . . 
+. . . f a a a 1 1 a a a f . . . 
+. . . . f a a 1 1 f f f f . . . 
+. . . f f f f f f e e e f f . . 
+. . . . f f f f f f f f f . . . 
+`;
+            case "GwAjxys0DjFjV^^:lML1":
+            case "Zelda_back_rock_alt":return img`
+. . . . . . . . . . . . . . . . 
+. . . f f . f f f f . f f . . . 
+. . f d d f c c c c f d d f . . 
+. . f d d f c c c c f d d f . . 
+. . f f f c c c f c c f f f . . 
+. . f 1 f c c f c c c f 1 f . . 
+. . f 1 f c c c c c c f 1 f . . 
+. . f 1 f c c c c c c f 1 f . . 
+. . f 1 f c c c c c c f 1 f . . 
+. . . f c 6 6 c 6 6 c f 1 f . . 
+. . . f c c 6 c 6 c c c f . . . 
+. . . f c 6 6 c 6 6 c c f . . . 
+. . . f c 6 c c c 6 c a f . . . 
+. . . f f f 6 c 6 6 a f f . . . 
+. . f f e e f f f f f f f . . . 
+. . . f f f f f f f f f . . . . 
+`;
+            case "image13":
+            case "Zelda_back_rock":return img`
+. . . . . . . . . . . . . . . . 
+. . . f f . f f f f . f f . . . 
+. . f d d f c c c c f d d f . . 
+. . f d d f c c c c f d d f . . 
+. . f f f c c f c c c f f f . . 
+. . f 1 f c c c f c c f 1 f . . 
+. . f 1 f c c c c c c f 1 f . . 
+. . f 1 f c c c c c c f 1 f . . 
+. . f 1 f c c c c c c f 1 f . . 
+. . f 1 f c 6 6 c 6 6 c f . . . 
+. . . f c c c 6 c 6 c c f . . . 
+. . . f c c 6 6 c 6 6 c f . . . 
+. . . f a c 6 c c c 6 c f . . . 
+. . . f f a 6 6 c 6 f f f . . . 
+. . . f f f f f f f e e f f . . 
+. . . . f f f f f f f f f . . . 
+`;
+            case "image12":
+            case "Zelda_front_rock_alt":return img`
+. . . . . f f f f f f . . . . . 
+. . . f f a c c c c c f f . . . 
+. . f d d f c c c c f d d f . . 
+. . f d d f 5 5 c c f d d f . . 
+. . f f f 5 5 5 5 5 c f f f . . 
+. . f 1 f 5 d d d d 5 f 1 f . . 
+. . f 1 f d f d d f d f 1 f . . 
+. . f 1 f d f d d f d f 1 f . . 
+. . f 1 f d d d d d d f 1 f . . 
+. . f 1 f c d 3 3 d c f f . . . 
+. . . f a f f f f f f a f . . . 
+. . . f a a a 1 1 a a a f . . . 
+. . . f a a a 1 1 a a a f . . . 
+. . . f f f f 1 1 a a f . . . . 
+. . f f e e e f f f f f f . . . 
+. . . f f f f f f f f f . . . . 
+`;
+            case "image3":
+            case "zelda_right":return img`
+. . . . . . f f f f f . . . . . 
+. . . . . f a a c c c f f . . . 
+. . . . f a c c c c c 5 5 f . . 
+. . . f a c c c c c 5 5 5 f . . 
+. . f a c c c c c 5 5 5 f f . . 
+. . f c c c c c c 5 f f f . . . 
+. . . f c c c c f f b f d f f . 
+. . . . f c c c f b d f d d f . 
+. . . . . f 6 c f b d d d f . . 
+. . . . . f 6 c f f d 3 f . . . 
+. . . . . f 6 f 1 1 f f f . . . 
+. . . . . f c f 1 1 f a f . . . 
+. . . . . f c f d d f a f . . . 
+. . . . . f f f f f f a f . . . 
+. . . . . f e e e e f f f . . . 
+. . . . f f f f f f f f f f . . 
+`;
+            case "image14":
+            case "zelda_right_rock":return img`
+. . . . . f f f f f . . . . . . 
+. . . . f a f f c c f f . . . . 
+. . . f a f d d f c 5 5 f . . . 
+. . f a c f d d f 5 5 5 f . . . 
+. f a c c f f f f 5 5 f f . . . 
+. f c c c f 1 1 f f f f . . . . 
+. . f c c f 1 1 f b f d f f . . 
+. . . f c f 1 1 f d f d d f . . 
+. . . . f f 1 1 f d d d f . . . 
+. . . . f f 1 1 f d 3 f . . . . 
+. . . . f 6 f f 1 f f f . . . . 
+. . . . f c c c 1 a a f . . . . 
+. . . . f c c c 1 a a f . . . . 
+. . . . f f f f f a a f . . . . 
+. . . . f e e e e f f f . . . . 
+. . . f f f f f f f f f f . . . 
+`;
+            case "image15":
+            case "Zelda_left_rock_alt":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . f f c c f f c f . . . . . 
+. . f 5 5 c f d d f c f . . . . 
+. . f 5 5 5 f d d f c c f . . . 
+. . f f 5 5 f f f f c c c f . . 
+. . . f f f f 1 1 f c c c f . . 
+. f f d f d f 1 1 f c c f . . . 
+. f d d f d f 1 1 f f f . . . . 
+. . f d d d f 1 1 f f . . . . . 
+. . . f 3 d f 1 1 f f . . . . . 
+. . . . f f a f f 6 f f . . . . 
+. . . . f a a 1 1 c f e f . . . 
+. . . f f f a 1 1 f f e f . . . 
+. . f e e e f f f f e e f . . . 
+. . f f f f f f f f f f f f . . 
+`;
+            case "image16":
+            case "carry_rock":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . . f b b b c c f . . . . . 
+. . . f b b b b c c c f . . . . 
+. . f b b b b b c c c c f . . . 
+. f b b b b b b c c c c c f . . 
+f b b b b b b b c c c c c c f . 
+f c b b b b b c b c c c b c f . 
+f c c b b b c c c b c b c f f . 
+f c c c b c c c c c b c f f f . 
+f c c c b c d d d c b f f f f . 
+f c c c b c d c d c b f f f f . 
+f f c c b c d d d c b f f f f . 
+. f f c b c d c d c b f f f . . 
+. . f f c c c c c c c f f . . . 
+. . . . f f f f f f f . . . . . 
+`;
             case "image10":
-            case "Rock":return img`
-................................................
-................................................
-................................................
-................................................
-................................................
-................................................
-................................................
-................................................
-................................................
-................................................
-................................................
-........................7.......................
-................................................
-................................................
-................................................
-.......................7........................
-................7777777777777777................
-................77777fffff777777................
-................7777fbbbccf77777................
-................777fbbbbcccf7777................
-................77fbbbbbccccf777................
-................7fbbbbbbcccccf77................
-................fbbbbbbbccccccf7................
-................fcbbbbbcbcccbcf7................
-...........7...7fccbbbcccbcbcff77...7...........
-................fcccbcccccbcfff7................
-................fcccbcdddcbffff7................
-................fcccbcdcdcbffff7................
-................ffccbcdddcbffff7................
-................7ffcbcdcdcbfff77................
-................77ffcccccccff777................
-................7777fffffff77776................
-........................7.......................
-................................................
-................................................
-........................7.......................
-................................................
-................................................
-................................................
-................................................
-................................................
-................................................
-................................................
-................................................
-................................................
-................................................
-................................................
-................................................
+            case "Zelda_left_rock":return img`
+. . . . . . f f f f f . . . . . 
+. . . . f f c c f f a f . . . . 
+. . . f 5 5 c f d d f a f . . . 
+. . . f 5 5 5 f d d f c a f . . 
+. . . f f 5 5 f f f f c c a f . 
+. . . . f f f f 1 1 f c c c f . 
+. . f f d f b f 1 1 f c c f . . 
+. . f d d f d f 1 1 f c f . . . 
+. . . f d d d f 1 1 f f . . . . 
+. . . . f 3 d f 1 1 f f . . . . 
+. . . . f f f 1 f f 6 f . . . . 
+. . . . f a a 1 c c c f . . . . 
+. . . . f a a 1 c c c f . . . . 
+. . . . f a a f f f f f . . . . 
+. . . . f f f e e e e f . . . . 
+. . . f f f f f f f f f f . . . 
 `;
         }
         return null;
--- main.ts
+++ main.ts
@@ -33,34 +33,52 @@ for (let value of tiles.getTilesByType(assets.tile`myTile`)) {
     Gustzol,
     assets.animation`Gust_zol_Animation`,
     650,
-    false
+    true
     )
+    Gustzol.follow(zelda, 20)
 }
 game.onUpdate(function () {
     if (steinsjekk == 1) {
         if (zelda.vx < 0) {
             if (Math.round(zelda.x / 10) % 2 == 0) {
-            	
+                zelda.setImage(assets.image`Zelda_left_rock`)
             } else {
-            	
+                zelda.setImage(assets.image`Zelda_left_rock_alt`)
             }
         } else if (zelda.vx > 0) {
             if (Math.round(zelda.x / 10) % 2 == 0) {
-            	
+                zelda.setImage(assets.image`zelda_right_rock`)
             } else {
-            	
+                zelda.setImage(img`
+                    . . . . . . . . . . . . . . . . 
+                    . . . . . . f f f f f . . . . . 
+                    . . . . . f c f f c c f f . . . 
+                    . . . . f c f d d f c 5 5 f . . 
+                    . . . f c c f d d f 5 5 5 f . . 
+                    . . f c c c f f f f 5 5 f f . . 
+                    . . f c c c f 1 1 f f f f . . . 
+                    . . . f c c f 1 1 f d f d f f . 
+                    . . . . f f f 1 1 f d f d d f . 
+                    . . . . . f f 1 1 f d d d f . . 
+                    . . . . . f f 1 1 f d 3 f . . . 
+                    . . . . f f 6 f f a f f . . . . 
+                    . . . f e f c 1 1 a a f . . . . 
+                    . . . f e f f 1 1 a f f f . . . 
+                    . . . f e e f f f f e e e f . . 
+                    . . f f f f f f f f f f f f . . 
+                    `)
             }
         } else if (zelda.vy > 0) {
             if (Math.round(zelda.y / 10) % 2 == 0) {
-            	
+                zelda.setImage(assets.image`Zelda_front_stone`)
             } else {
-            	
+                zelda.setImage(assets.image`Zelda_front_rock_alt`)
             }
         } else if (zelda.vy < 0) {
             if (0 == Math.round(zelda.y / 10) % 2) {
-            	
+                zelda.setImage(assets.image`Zelda_back_rock`)
             } else {
-            	
+                zelda.setImage(assets.image`Zelda_back_rock_alt`)
             }
         } else {
         	
