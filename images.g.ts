972315e1c0adb59b73e1dc4cd530dff61fa70217
Sverre02 <sverre.rommetveit.landheim@gmail.com>
2025-02-16 21:50:28 +0100

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -41,25 +41,6 @@ namespace myImages {
 . . . f e e f f f e e e e f . . 
 . . f f f f f f f f f f f f . . 
 `;
-            case "image7":
-            case "Zelda_back":return img`
-. . . . . . . . . . . . . . . . 
-. . . . . . f f f f . . . . . . 
-. . . . . f c c c c f . . . . . 
-. . . . f c c c c c c f . . . . 
-. . . f f c c c f c c c f . . . 
-. . . f c c c f c c c c f . . . 
-. . . f c c c c c c c c f . . . 
-. . . f c c c c c c c a f . . . 
-. . . f f c c c c c a f f . . . 
-. . f c f 6 6 c 6 6 f c f . . . 
-. . f a f c 6 c 6 c f a d f . . 
-. . f f c 6 6 c 6 6 c f d f . . 
-. . . f c 6 c c c 6 c a f . . . 
-. . . f f f 6 c 6 6 a f f . . . 
-. . f f e e f f f f f f f . . . 
-. . . f f f f f f f f f . . . . 
-`;
             case "image8":
             case "Zelda_back_alt":return img`
 . . . . . . . . . . . . . . . . 
@@ -326,24 +307,268 @@ namespace myImages {
 . . . f f c c c c c c c f f . . 
 . . . . . f f f f f f f . . . . 
 `;
+            case "NCV(:-%h:[|8ahlwtB9+":
+            case "zol":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . f 5 5 5 5 5 5 f . . . . 
+. . . f 5 5 5 1 1 5 5 5 f . . . 
+. . f 5 5 5 1 1 1 1 5 5 5 f . . 
+. . f 9 5 5 5 1 1 5 5 5 9 f . . 
+. . f 5 9 5 5 5 5 5 5 9 5 f . . 
+. . f 9 5 5 f 5 5 f 5 5 9 f . . 
+. . f 5 9 5 f 5 5 f 5 9 5 f . . 
+. . f f 5 5 5 5 5 5 5 5 f f . . 
+. . . f f 5 5 5 5 5 5 f f . . . 
+. . . . f f f f f f f f . . . . 
+`;
+            case "4,rP$1!KG+4bDJ|i1?i^":
+            case "zelda_right_rock_alt":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f f . . . . . 
+. . . . . f c f f c c f f . . . 
+. . . . f c f d d f c 5 5 f . . 
+. . . f c c f d d f 5 5 5 f . . 
+. . f c c c f f f f 5 5 f f . . 
+. . f c c c f 1 1 f f f f . . . 
+. . . f c c f 1 1 f d f d f f . 
+. . . . f f f 1 1 f d f d d f . 
+. . . . . f f 1 1 f d d d f . . 
+. . . . . f f 1 1 f d 3 f . . . 
+. . . . f f 6 f f a f f . . . . 
+. . . f e f c 1 1 a a f . . . . 
+. . . f e f f 1 1 a f f f . . . 
+. . . f e e f f f f e e e f . . 
+. . f f f f f f f f f f f f . . 
+`;
+            case "image18":
+            case "Henry_the_hermit_underground":return img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 6 7 7 7 7 7 b 7 7 7 7 
+7 7 b 7 7 7 7 7 b 7 b 7 7 7 7 7 
+7 6 7 b 7 7 7 b 7 b 7 b 7 7 7 7 
+7 7 7 7 b 7 b 4 b 7 7 7 7 7 7 7 
+7 7 7 7 7 b 4 1 4 b 7 7 6 7 7 7 
+7 7 7 7 7 b 4 1 4 b 7 7 7 7 7 7 
+7 7 6 7 7 7 b b b 7 7 7 7 7 7 7 
+7 7 7 7 7 b 7 7 7 b 7 7 7 7 7 7 
+7 7 7 7 b 7 7 7 b 6 b 7 7 7 7 7 
+7 7 7 b 7 7 7 7 7 7 7 b 7 6 7 7 
+7 7 b 6 b 7 7 7 7 7 7 7 b 7 7 7 
+7 7 7 7 7 b 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 
+`;
+            case "image7":
+            case "Zelda_back":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . f c c c c f . . . . . 
+. . . . f c c c c c c f . . . . 
+. . . f f c c c f c c c f . . . 
+. . . f c c c f c c c c f . . . 
+. . . f c c c c c c c c f . . . 
+. . . f c c c c c c c a f . . . 
+. . . f f c c c c c a f f . . . 
+. . f c f 6 6 c 6 6 f c f . . . 
+. . f a f c 6 c 6 c f a d f . . 
+. . f f c 6 6 c 6 6 c f d f . . 
+. . . f c 6 c c c 6 c a f . . . 
+. . . f f f 6 c 6 6 a f f . . . 
+. . f f e e f f f f f f f . . . 
+. . . f f f f f f f f f . . . . 
+`;
+            case "nVV+IW-**e5N@Gf2r]lO":
+            case "Zelda_back_suprised":return img`
+. . . . . . f f f f . . . . 2 . 
+. . . . . f c c c c f . . . 2 . 
+. . . . f c c c c c c f . . 2 . 
+. . . f f c c c f c c c f . . . 
+. . . f c c c f c c c c f . 2 . 
+. . . f c c c c c c c c f . . . 
+. . . f c c c c c c c a f . . . 
+. . . f f c c c c c a f f . . . 
+. . f c f 6 6 c 6 6 f c f . . . 
+. . f a f c 6 c 6 c f a d f . . 
+. . f f c 6 6 c 6 6 c f d f . . 
+. . . f c 6 c c c 6 c a f . . . 
+. . . f f f 6 c 6 6 a f f . . . 
+. . f f e e f f f f f f f . . . 
+. . . f f f f f f f f f . . . . 
+. . . . . . . . . . . . . . . . 
+`;
             case "image17":
             case "Henry_the_hermit":return img`
-7 7 7 7 7 7 6 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 7 7 7 7 7 6 7 7 7 7 7 
-7 6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-7 7 7 7 7 f f f f f 7 7 7 7 6 7 
-7 7 7 f f f d d d f f f 7 7 7 7 
-7 7 f f f d d d d d f f f 7 7 7 
-7 f f f d f f f f f d f f f 7 7 
-f f f d f 4 4 4 4 4 f d f f f 7 
-f f d 4 f 4 4 4 4 4 f e d f f 7 
-f d 4 4 f 4 1 4 1 4 f 4 4 f f 7 
-f 4 4 4 f 4 1 4 1 4 f 4 4 e f 7 
-f 4 f 4 4 f 4 4 4 f 4 4 f e f 7 
-f f f 4 4 4 f f f 4 4 e f f f 7 
-7 f f f 4 4 f f f 4 e f f f 7 7 
-7 7 7 7 4 4 7 7 7 4 e 7 7 7 7 7 
-6 7 7 7 7 7 7 7 7 7 7 7 7 7 6 7 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f . . . . . . 
+. . . f f f d d d f f f . . . . 
+. . f f f d d d d d f f f . . . 
+. f f f d f f f f f d f f f . . 
+f f f d f 4 4 4 4 4 f d f f f . 
+f f d 4 f 4 4 4 4 4 f e d f f . 
+f d 4 4 f 4 1 4 1 4 f 4 4 f f . 
+f 4 4 4 f 4 1 4 1 4 f 4 4 e f . 
+f 4 f 4 4 f 4 4 4 f 4 4 f e f . 
+. f f 4 4 4 f f f 4 4 e f f . . 
+. . f f 4 4 f f f 4 e f f . . . 
+. . . . 4 4 . . . 4 e . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "#B$mej{90gf3fld?8Fc7":
+            case "empty":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "w}^2U!tfG;)%/0%pj]ja":
+            case "Henry_plate":return img`
+555555555555555555555555
+544444444444444444444445
+545555555555555555555545
+555555555555555555555555
+252442555555555555244252
+252552555555555555255252
+252222555555555555222252
+255555555555555555555552
+255555555555555555555552
+255555555555555555555552
+255555555555555555555552
+255555555555555555555552
+255555555555555555555552
+255555555555555555555552
+255555555555555555555552
+255555555555555555555552
+255555555555555555555552
+255555555555555555555552
+252442555555555555244252
+252552555555555555255252
+252222555555555555222252
+225555555555555555555522
+222222222222222222222222
+222222222222222222222222
+`;
+            case "image19":
+            case "key":return img`
+. . . . . 5 5 5 5 5 . . . . . . 
+. . . . 5 5 5 5 5 5 5 . . . . . 
+. . . . 4 5 . . 4 5 5 . . . . . 
+. . . . 4 5 . . 4 5 5 . . . . . 
+. . . . 4 5 . . 4 5 5 . . . . . 
+. . . . 4 5 5 5 5 5 5 . . . . . 
+. . . . . 4 5 5 5 5 . . . . . . 
+. . . . . . 4 5 5 . . . . . . . 
+. . . . . . 4 5 5 . . . . . . . 
+. . . . . . 4 5 5 . . . . . . . 
+. . . . . . 4 5 5 . . . . . . . 
+. . . . . . 4 5 5 5 5 . . . . . 
+. . . . . . 4 5 5 5 5 . . . . . 
+. . . . . . 4 5 5 . . . . . . . 
+. . . . . . 4 5 5 5 5 5 . . . . 
+. . . . . . 4 5 5 . . . . . . . 
+`;
+            case "image20":
+            case "Gate":return img`
+. . . . . . . . . . . . . . . . 
+. . . . d . d d d d . d . . . . 
+. . . d d . d d d d . d d . . . 
+. . d d d . d d d d . d d d . . 
+. . d d d . d d d d . d d d . . 
+. . d d d . b b b b . d d d . . 
+. . d d d b 2 b b 2 b d d d . . 
+. . d d d b 2 b b 2 b d d d . . 
+. . d d d b b b b b b d d d . . 
+. . d d d . b f f b . d d d . . 
+. . d d d . b b b b . d d d . . 
+. . d d d . d d d d . d d d . . 
+. . d d d . d d d d . d d d . . 
+. . d d d . d d d d . d d d . . 
+. . d d d . d d d d . d d d . . 
+. . . d . . . d d . . . d . . . 
+`;
+            case "1^:]@H?|3c|Cv-Qg~|}Z":
+            case "Wand":return img`
+. . . . . . . 5 . . . . . . . . 
+. . . . . . 5 5 5 . . . . . . . 
+. . . 5 5 . . 5 . . 7 7 . . . . 
+. . . . 5 5 . . . 7 7 . . . . . 
+. . . . . 5 5 7 7 7 . . . . . . 
+. . . . . . 5 5 7 . . . . . . . 
+. . . . . . 7 5 5 . . . . . . . 
+. . . . . . 7 7 5 . . . . . . . 
+. . . . . . 5 7 7 . . . . . . . 
+. . . . . . 5 5 7 . . . . . . . 
+. . . . . . 7 5 5 . . . . . . . 
+. . . . . . 7 7 5 . . . . . . . 
+. . . . . . 5 7 7 . . . . . . . 
+. . . . . . 5 5 7 . . . . . . . 
+. . . . . . 7 5 5 . . . . . . . 
+. . . . . . . 7 . . . . . . . . 
+`;
+            case "image21":
+            case "Button":return img`
+. . . . e e e e e e e . . . . 
+. . e e 5 5 5 5 5 5 5 e e . . 
+. e e 5 5 5 2 2 2 5 5 5 e e . 
+. e 5 5 5 2 2 5 2 2 5 5 5 e . 
+. c 5 5 2 2 2 2 2 2 2 5 5 c . 
+. c 4 5 2 5 5 5 5 5 2 5 4 c . 
+. c 4 4 5 5 5 5 5 5 5 4 4 c . 
+. c e e 4 4 4 4 4 4 4 e e c . 
+c c e e e e e e e e e e e c c 
+c d c c e e e e e e e c c d c 
+c d d d c c c c c c c d d d c 
+c c b d d d d d d d d d b c c 
+c c c c c b b b b b c c c c c 
+c c b b b b b b b b b b b c c 
+. c c b b b b b b b b b c c . 
+. . . c c c c c c c c c . . . 
+`;
+            case "image22":
+            case "Info_box":return img`
+..dddddddddddddddddddd..
+.dbbddbbddbbddbbddbbddd.
+dddbbddbbddbbddbbddbbdbd
+ddbddddddddddddddddddbbd
+dbbdd11111111111111ddbdd
+dbdd1111111111111111dddd
+dddd1111111111111111ddbd
+ddbd1111111111111111dbbd
+dbbd1111111111111111dbdd
+dbdd1111111111111111dddd
+dddd1111111111111111ddbd
+ddbd1111111111111111dbbd
+dbbd1111111111111111dbdd
+dbdd1111111111111111dddd
+dddd1111111111111111ddbd
+ddbd1111111111111111dbbd
+dbbd1111111111111111dbdd
+dbdd1111111111111111dddd
+dddd1111111111111111ddbd
+ddbdd11111111111111ddbbd
+dbbddddddddddddddddddbdd
+dbdbbddbbddbbddbbddbbddd
+.dddbbddbbddbbddbbddbbd.
+..dddddddddddddddddddd..
 `;
         }
         return null;
@@ -524,6 +749,129 @@ f f f 4 4 4 f f f 4 4 e f f f 7
 . 5 5 f f 5 5 5 5 5 5 f f 5 5 . 
 . . 5 5 f f f f f f f f 5 5 . . 
 . . . 5 5 5 5 5 5 5 5 5 5 . . . 
+`];
+            case "Zelda_suprised_animation":
+            case "anim3":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . f c c c c f . . . . . 
+. . . . f c c c c c c f . . . . 
+. . . f f c c c f c c c f . . . 
+. . . f c c c f c c c c f . . . 
+. . . f c c c c c c c c f . . . 
+. . . f c c c c c c c a f . . . 
+. . . f f c c c c c a f f . . . 
+. . f c f 6 6 c 6 6 f c f . . . 
+. . f a f c 6 c 6 c f a d f . . 
+. . f f c 6 6 c 6 6 c f d f . . 
+. . . f c 6 c c c 6 c a f . . . 
+. . . f f f 6 c 6 6 a f f . . . 
+. . f f e e f f f f f f f . . . 
+. . . f f f f f f f f f . . . . 
+`, img`
+. . . . . . f f f f . . . . 2 . 
+. . . . . f c c c c f . . . 2 . 
+. . . . f c c c c c c f . . 2 . 
+. . . f f c c c f c c c f . . . 
+. . . f c c c f c c c c f . 2 . 
+. . . f c c c c c c c c f . . . 
+. . . f c c c c c c c a f . . . 
+. . . f f c c c c c a f f . . . 
+. . f c f 6 6 c 6 6 f c f . . . 
+. . f a f c 6 c 6 c f a d f . . 
+. . f f c 6 6 c 6 6 c f d f . . 
+. . . f c 6 c c c 6 c a f . . . 
+. . . f f f 6 c 6 6 a f f . . . 
+. . f f e e f f f f f f f . . . 
+. . . f f f f f f f f f . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . 2 . 
+. . . . . f c c c c f . . . 2 . 
+. . . . f c c c c c c f . . 2 . 
+. . . f f c c c f c c c f . . . 
+. . . f c c c f c c c c f . 2 . 
+. . . f c c c c c c c c f . . . 
+. . . f c c c c c c c a f . . . 
+. . . f f c c c c c a f f . . . 
+. . f c f 6 6 c 6 6 f c f . . . 
+. . f a f c 6 c 6 c f a d f . . 
+. . f f c 6 6 c 6 6 c f d f . . 
+. . . f c 6 c c c 6 c a f . . . 
+. . . f f f 6 c 6 6 a f f . . . 
+. . f f e e f f f f f f f . . . 
+. . . f f f f f f f f f . . . . 
+`];
+            case "wand animation":
+            case "anim4":return [img`
+. . . . . . . 5 . . . . . . . . 
+. . . . . . 5 5 5 . . . . . . . 
+. . . 5 5 . . 5 . . 7 7 . . . . 
+. . . . 5 5 . . . 7 7 . . . . . 
+. . . . . 5 5 . 7 7 . . . . . . 
+. . . . . . 5 5 7 . . . . . . . 
+. . . . . . 7 5 5 . . . . . . . 
+. . . . . . 7 7 5 . . . . . . . 
+. . . . . . 5 7 7 . . . . . . . 
+. . . . . . 5 5 7 . . . . . . . 
+. . . . . . 7 5 5 . . . . . . . 
+. . . . . . 7 7 5 . . . . . . . 
+. . . . . . 5 7 7 . . . . . . . 
+. . . . . . 5 5 7 . . . . . . . 
+. . . . . . 7 5 5 . . . . . . . 
+. . . . . . . 7 . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . 5 . . . . . . . . 
+. . . 5 5 . . . . . 7 7 . . . . 
+. . . . 5 5 . . . 7 7 . . . . . 
+. . . . . 5 5 . 7 7 . . . . . . 
+. . . . . . 5 5 7 . . . . . . . 
+. . . . . . 7 5 5 . . . . . . . 
+. . . . . . 7 7 5 . . . . . . . 
+. . . . . . 5 7 7 . . . . . . . 
+. . . . . . 5 5 7 . . . . . . . 
+. . . . . . 7 5 5 . . . . . . . 
+. . . . . . 7 7 5 . . . . . . . 
+. . . . . . 5 7 7 . . . . . . . 
+. . . . . . 5 5 7 . . . . . . . 
+. . . . . . 7 5 5 . . . . . . . 
+. . . . . . . 7 . . . . . . . . 
+`, img`
+. . . . . . 5 . 5 . . . . . . . 
+. . . . . . . 5 . . . . . . . . 
+. . . 5 5 . 5 . 5 . 7 7 . . . . 
+. . . . 5 5 . . . 7 7 . . . . . 
+. . . . . 5 5 . 7 7 . . . . . . 
+. . . . . . 5 5 7 . . . . . . . 
+. . . . . . 7 5 5 . . . . . . . 
+. . . . . . 7 7 5 . . . . . . . 
+. . . . . . 5 7 7 . . . . . . . 
+. . . . . . 5 5 7 . . . . . . . 
+. . . . . . 7 5 5 . . . . . . . 
+. . . . . . 7 7 5 . . . . . . . 
+. . . . . . 5 7 7 . . . . . . . 
+. . . . . . 5 5 7 . . . . . . . 
+. . . . . . 7 5 5 . . . . . . . 
+. . . . . . . 7 . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . 5 . . . . . . . . 
+. . . 5 5 . . . . . 7 7 . . . . 
+. . . . 5 5 . . . 7 7 . . . . . 
+. . . . . 5 5 . 7 7 . . . . . . 
+. . . . . . 5 5 7 . . . . . . . 
+. . . . . . 7 5 5 . . . . . . . 
+. . . . . . 7 7 5 . . . . . . . 
+. . . . . . 5 7 7 . . . . . . . 
+. . . . . . 5 5 7 . . . . . . . 
+. . . . . . 7 5 5 . . . . . . . 
+. . . . . . 7 7 5 . . . . . . . 
+. . . . . . 5 7 7 . . . . . . . 
+. . . . . . 5 5 7 . . . . . . . 
+. . . . . . 7 5 5 . . . . . . . 
+. . . . . . . 7 . . . . . . . . 
 `];
         }
         return null;
--- main.ts
+++ main.ts
@@ -2,6 +2,10 @@ namespace SpriteKind {
     export const Zol = SpriteKind.create()
     export const Throwable = SpriteKind.create()
     export const Echo_zol = SpriteKind.create()
+    export const npc = SpriteKind.create()
+    export const bar = SpriteKind.create()
+    export const wand = SpriteKind.create()
+    export const UI = SpriteKind.create()
 }
 scene.onHitWall(SpriteKind.Player, function (sprite, location) {
     if (controller.A.isPressed() && steinsjekk == 0) {
@@ -27,49 +31,40 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Echo_zol, function (sprite, otherSprite) {
     animation.stopAnimation(animation.AnimationTypes.All, zol_echo_shimmer)
-    zol_echo_shimmer.setImage(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
+    effects.clearParticles(zol_echo_shimmer)
+    zol_echo_shimmer.setImage(assets.image`empty`)
+    game.setDialogFrame(img`
+        ..bbbbbbbbbbbbbbbbbbbb..
+        .b11bb11bb11bb11bb11bbb.
+        bbb11bb11bb11bb11bb11b1b
+        bb1bbbbbbbbbbbbbbbbbb11b
+        b11bb11111111111111bb1bb
+        b1bb1111111111111111bbbb
+        bbbb1111111111111111bb1b
+        bb1b1111111111111111b11b
+        b11b1111111111111111b1bb
+        b1bb1111111111111111bbbb
+        bbbb1111111111111111bb1b
+        bb1b1111111111111111b11b
+        b11b1111111111111111b1bb
+        b1bb1111111111111111bbbb
+        bbbb1111111111111111bb1b
+        bb1b1111111111111111b11b
+        b11b1111111111111111b1bb
+        b1bb1111111111111111bbbb
+        bbbb1111111111111111bb1b
+        bb1bb11111111111111bb11b
+        b11bbbbbbbbbbbbbbbbbb1bb
+        b1b11bb11bb11bb11bb11bbb
+        .bbb11bb11bb11bb11bb11b.
+        ..bbbbbbbbbbbbbbbbbbbb..
         `)
-    game.showLongText("Hey listen!you found a echo!", DialogLayout.Bottom)
-    game.showLongText("This is how you use it:", DialogLayout.Bottom)
-    game.showLongText("Press B to spawn an echo.", DialogLayout.Bottom)
-    game.showLongText("Hold B to Destroy echoes.", DialogLayout.Bottom)
+    game.showLongText("You found a echo!", DialogLayout.Bottom)
+    has_voltzol_echo += 1
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (steinsjekk == 1) {
-        Rock.setImage(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `)
+        Rock.setImage(assets.image`empty`)
         steinsjekk = 0
         if (direction == 0) {
             projectile = sprites.createProjectileFromSprite(assets.image`carry_rock`, Rock, -50, 10)
@@ -88,13 +83,6 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         sprites.destroy(projectile)
     }
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Zol, function (sprite, otherSprite) {
-    info.changeLifeBy(-1)
-    zelda.startEffect(effects.warmRadial, 100)
-    otherSprite.follow(zelda, 0)
-    pause(1000)
-    otherSprite.follow(zelda, 20)
-})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     direction = 0
 })
@@ -104,92 +92,88 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     direction = 3
 })
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Zol, function (sprite, otherSprite) {
-    if (zol_kill_count == 0) {
-        zol_echo_shimmer.setPosition(otherSprite.x, otherSprite.y)
-        zol_echo_shimmer.setImage(img`
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . f 5 5 5 5 5 5 f . . . . 
-            . . . f 5 5 5 1 1 5 5 5 f . . . 
-            . . f 5 5 5 1 1 1 1 5 5 5 f . . 
-            . . f 9 5 5 5 1 1 5 5 5 9 f . . 
-            . . f 5 9 5 5 5 5 5 5 9 5 f . . 
-            . . f 9 5 5 f 5 5 f 5 5 9 f . . 
-            . . f 5 9 5 f 5 5 f 5 9 5 f . . 
-            . . f f 5 5 5 5 5 5 5 5 f f . . 
-            . . . f f 5 5 5 5 5 5 f f . . . 
-            . . . . f f f f f f f f . . . . 
-            `)
-        zol_echo_shimmer.startEffect(effects.starField)
-        animation.runImageAnimation(
-        zol_echo_shimmer,
-        assets.animation`Zol_echo_shimmer_animation`,
-        500,
-        true
-        )
+sprites.onOverlap(SpriteKind.Player, SpriteKind.wand, function (sprite, otherSprite) {
+    sprites.destroy(wand2)
+    game.setDialogFrame(assets.image`Info_box`)
+    game.showLongText("You Got a wand", DialogLayout.Bottom)
+    animation.runImageAnimation(
+    bar2,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `],
+    500,
+    false
+    )
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    if (otherSprite == woltzol) {
+        if (zol_kill_count == 0) {
+            zol_echo_shimmer = sprites.create(assets.image`zol`, SpriteKind.Echo_zol)
+            zol_echo_shimmer.setPosition(otherSprite.x, otherSprite.y)
+            zol_echo_shimmer.setImage(assets.image`zol`)
+            zol_echo_shimmer.startEffect(effects.starField)
+            animation.runImageAnimation(
+            zol_echo_shimmer,
+            assets.animation`Zol_echo_shimmer_animation`,
+            500,
+            true
+            )
+        }
+        zol_kill_count += 1
     }
     sprites.destroy(otherSprite, effects.disintegrate, 100)
     sprites.destroy(sprite, effects.ashes, 100)
-    zol_kill_count += 1
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    zelda.startEffect(effects.fire, 100)
+    otherSprite.follow(zelda, 0)
+    if (otherSprite == woltzol) {
+        pause(1000)
+        otherSprite.follow(zelda, 20)
+    }
 })
 let zol_kill_count = 0
+let wand2: Sprite = null
 let projectile: Sprite = null
+let has_voltzol_echo = 0
+let zol_echo_shimmer: Sprite = null
 let direction = 0
+let steinsjekk = 0
 let woltzol: Sprite = null
 let zelda: Sprite = null
-let steinsjekk = 0
 let Rock: Sprite = null
-let zol_echo_shimmer: Sprite = null
-zol_echo_shimmer = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Echo_zol)
-Rock = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Throwable)
-steinsjekk = 0
+let bar2: Sprite = null
+let A = sprites.create(assets.image`empty`, SpriteKind.UI)
+bar2 = sprites.create(assets.image`Gate`, SpriteKind.bar)
+let Henry_the_hermit = sprites.create(assets.image`empty`, SpriteKind.npc)
+Rock = sprites.create(assets.image`empty`, SpriteKind.Throwable)
 zelda = sprites.create(assets.image`Zelda_front`, SpriteKind.Player)
 scene.cameraFollowSprite(zelda)
 tiles.setCurrentTilemap(tilemap`level2`)
+A.setFlag(SpriteFlag.RelativeToCamera, true)
+A.setPosition(150, 109)
 Rock.setFlag(SpriteFlag.GhostThroughWalls, true)
 info.setLife(5)
-for (let value of tiles.getTilesByType(assets.tile`myTile`)) {
-    woltzol = sprites.create(assets.image`Gust_zol`, SpriteKind.Zol)
-    tiles.placeOnTile(woltzol, value)
+tiles.placeOnRandomTile(bar2, assets.tile`wall_top0`)
+for (let value2 of tiles.getTilesByType(assets.tile`myTile`)) {
+    woltzol = sprites.create(assets.image`Gust_zol`, SpriteKind.Enemy)
+    tiles.placeOnTile(woltzol, value2)
     animation.runImageAnimation(
     woltzol,
     assets.animation`Gust_zol_Animation`,
@@ -199,6 +183,34 @@ for (let value of tiles.getTilesByType(assets.tile`myTile`)) {
     woltzol.follow(zelda, 20)
 }
 game.onUpdate(function () {
+    A.setImage(assets.image`empty`)
+    if (1 == zol_kill_count && has_voltzol_echo == 1) {
+        Henry_the_hermit = sprites.create(assets.image`Henry_the_hermit`, SpriteKind.npc)
+        zol_kill_count = 0
+        tiles.placeOnRandomTile(Henry_the_hermit, assets.tile`myTile1`)
+        Henry_the_hermit.setImage(assets.image`Henry_the_hermit`)
+        has_voltzol_echo = 5
+    }
+    if (tiles.tileIs(tiles.locationInDirection(tiles.locationOfSprite(zelda), CollisionDirection.Top), assets.tile`myTile1`)) {
+        game.setDialogFrame(assets.image`Henry_plate`)
+        A.setImage(assets.image`Button`)
+        if (controller.A.isPressed() && 5 == has_voltzol_echo) {
+            has_voltzol_echo += 1
+            game.showLongText("Hello there. Thanks for taking care of Those pesky zols. My name is Henry the hermit. Vendor of rare goods. As a thank you i will open the door over there", DialogLayout.Bottom)
+            game.showLongText("Hey is that a echo shimmer. If you have a wand you can use it to have a enemy help you fight. HUH! you don't have a wand. Convenient that i have one in my stock right now. Do you want it?", DialogLayout.Bottom)
+            wand2 = sprites.create(assets.image`Wand`, SpriteKind.wand)
+            wand2.setPosition(Henry_the_hermit.left - 6, Henry_the_hermit.y)
+            animation.runImageAnimation(
+            wand2,
+            assets.animation`wand animation`,
+            1000,
+            true
+            )
+        }
+        if (controller.A.isPressed() && 6 == has_voltzol_echo) {
+            game.showLongText("Go ahead try it ", DialogLayout.Bottom)
+        }
+    }
     if (steinsjekk == 1) {
         controller.moveSprite(zelda, 27, 27)
         Rock.setPosition(zelda.x, zelda.top - 6)
@@ -212,24 +224,7 @@ game.onUpdate(function () {
             if (Math.round(zelda.x / 10) % 2 == 0) {
                 zelda.setImage(assets.image`zelda_right_rock`)
             } else {
-                zelda.setImage(img`
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . f f f f f . . . . . 
-                    . . . . . f c f f c c f f . . . 
-                    . . . . f c f d d f c 5 5 f . . 
-                    . . . f c c f d d f 5 5 5 f . . 
-                    . . f c c c f f f f 5 5 f f . . 
-                    . . f c c c f 1 1 f f f f . . . 
-                    . . . f c c f 1 1 f d f d f f . 
-                    . . . . f f f 1 1 f d f d d f . 
-                    . . . . . f f 1 1 f d d d f . . 
-                    . . . . . f f 1 1 f d 3 f . . . 
-                    . . . . f f 6 f f a f f . . . . 
-                    . . . f e f c 1 1 a a f . . . . 
-                    . . . f e f f 1 1 a f f f . . . 
-                    . . . f e e f f f f e e e f . . 
-                    . . f f f f f f f f f f f f . . 
-                    `)
+                zelda.setImage(assets.image`zelda_right_rock_alt`)
             }
         } else if (zelda.vy > 0) {
             if (Math.round(zelda.y / 10) % 2 == 0) {
